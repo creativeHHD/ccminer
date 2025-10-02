b48912f001aa818a7cfe894dd9488ee0ffb727a7
creativeHHD <92228259+creativeHHD@users.noreply.github.com>
2025-10-03 00:29:09 +0700

    Update startactive.py

--- startactive.py
+++ startactive.py
@@ -3,7 +3,7 @@ from progress.spinner import MoonSpinner
 # banner
 setting_banner = """
 ╔════════════════════════════════════╦════════╗
-║    ████ ████ █████ █ █    ██ █████ ║  V 2.2 ║
+║    ████ ████ █████ █ █    ██ █████ ║  V 2.5 ║
 ║   ██  █ █      █   █ █   ██        ║CREATIVE║
 ║  ██ ███ █      █   █ █  ██   ███   ║   HD   ║
 ║ ██    █ █      █   █ █ ██          ╠════════╣
@@ -12,7 +12,7 @@ setting_banner = """
 
 running_banner = """
 ╔════════════════════════════════════╦════════╗
-║    ████ ████ █████ █ █    ██ █████ ║  V 2.2 ║
+║    ████ ████ █████ █ █    ██ █████ ║  V 2.5 ║
 ║   ██  █ █      █     █   ██        ║CREATIVE║
 ║  ██ ███ █      █   █ █  ██   ███   ║   HD   ║
 ║ ██    █ █      █   █ █ ██          ╠════════╣
@@ -22,15 +22,16 @@ running_banner = """
 # banner function
 def banner(logo):
     os.system("clear")
-    print(logo,"\nDevelobment by CREATIVE-HD")
+    print(logo,"\nDevelobment AMS by CREATIVE-HD")
     print("------------------------------------------------") 
     print("                   ACTIVE MODE\n"
         + "    ▝  ▘\n"
         + " █ ███████ █     TERMUX AUTO START \n "
         + "█ █ 2.2 █ █ CCMINER AFTER BOOT DEVICE\n"
         + "   ███████       RUNNING AUTOMATIC\n"
-        + "    ██ ██                             DEC.2021")
+        + "    ██ ██                             AUG.2025")
     print("------------------------------------------------\n")
 
 
     
+
